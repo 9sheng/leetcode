ce6f7a2f4e99fa333273aee045ecae2b9261687f
Richie Liu <spliun60@126.com>
2015-03-06 14:01:46 +0800

    more solutions: 6, 67

---
+++ 6.cpp
@@ -0,0 +1,49 @@
+﻿#include "testharness.h"
+#include <map>
+#include <string>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+
+// 1,            9,              17
+// 2,        8,  10,         16, 18
+// 3,     7,     11,     15,     19
+// 4,  6,        12, 14,         20
+// 5,            13,             21
+
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        int step = 2 * (nRows - 1);
+        if (step == 0) return s;
+
+        string result = s;
+        int current = 0;
+
+        for (int row = 0; row < nRows; row++) {
+            int first = row;
+            int second = (nRows - 1) * 2 - row;
+
+            while (first < s.size()) {
+                result[current++] = s[first];
+
+                if (row != 0 && row != nRows - 1 && second < s.size())
+                    result[current++] = s[second];
+
+                first += step;
+                second += step;
+            }
+        }
+
+        return result;
+    }
+};
+
+TEST(Solution, test) {
+    ASSERT_EQ("PAHNAPLSIIGYIR", convert("PAYPALISHIRING", 3));
+    ASSERT_EQ("ACB", convert("ABC", 2));
+    ASSERT_EQ("ACBD", convert("ABCD", 2));
+    ASSERT_EQ("A", convert("A", 1));
+}
---
+++ 67.cpp
@@ -0,0 +1,72 @@
+﻿#include "testharness.h"
+#include <map>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string& result = a.size() > b.size() ? a : b;
+        string& tmp = a.size() > b.size() ? b : a;
+
+        int rs = result.size();
+        int ts = tmp.size();
+
+        bool hasCarryBit = false;
+
+        while (ts >= 0) {
+            if (result[rs] == '1') {
+                if (tmp[ts] == '1') {
+                    if (!hasCarryBit) {
+                        result[rs] = '0';
+                        hasCarryBit = true;
+                    }
+                } else {
+                    if (hasCarryBit) {
+                        result[rs] = '0';
+                    }
+                }
+            } else {
+                if (tmp[ts] == '1') {
+                    result[rs] = hasCarryBit ? '0' : '1';
+                } else {
+                    if (hasCarryBit) {
+                        result[rs] = '1';
+                        hasCarryBit = false;
+                    }
+                }
+            }
+
+            rs--;
+            ts--;
+        }
+
+        for (; rs >= 0; rs--) {
+            if (result[rs] == '1') {
+                if (hasCarryBit) {
+                    result[rs] = '0';
+                } else {
+                    break;
+                }
+            } else {
+                if (hasCarryBit) {
+                    result[rs] = '1';
+                    hasCarryBit = false;
+                }
+                break;
+            }
+        }
+
+        return hasCarryBit ? "1" + result : result;
+    }
+};
+
+TEST(Solution, test) {
+    ASSERT_EQ("100", addBinary("11", "1"));
+    ASSERT_EQ("100", addBinary("1", "11"));
+    ASSERT_EQ("110", addBinary("11", "11"));
+}
